983db942b29d093bcaa662dcf47385879cff847b
Aerdellosi <johnhanifin.webdev@gmail.com>
2023-01-13 12:21:46 -0800

    added a doc to print with a thumb drive -- VIM cheat sheet

--- finalgrade.js
+++ finalgrade.js
@@ -1,31 +1,45 @@
-const projectsCompleted = 7;
-const examScore = 55;
+ 
+window.onload = function() {
 
-function finalGrade(projectsCompleted, examScore) {
-    let gradeCheck = 0;
-    let finalGradeReturn = "";
+    const finalGradeForm = document.querySelector(".inputFromTeacher");
 
-    if (projectsCompleted > 10 || examScore > 90) {
-        gradeCheck = 3;
-    } else if (projectsCompleted >= 5 && examScore > 75) {
-        gradeCheck = 2;
-    } else if (projectsCompleted > 2 && examScore > 50) {
-        gradeCheck = 1;
-    }
+    finalGradeForm.addEventListener("submit", function(event) {
+        event.preventDefault();
 
-    if (gradeCheck === 3) {
-        finalGradeReturn = "final grade: 100";
-    } else if (gradeCheck === 2) {
-        finalGradeReturn = "final grade: 90";
-    } else if (gradeCheck === 1) {
-        finalGradeReturn = "final grade: 75";
-    } else {
-        finalGradeReturn = "final grade: 0";
-    }
 
-    return finalGradeReturn;
+        const projectsCompleted = document.getElementById("projectsCompleted").value;
+        const examScore = document.getElementById("examScore").value;
+
+        const finalGradeVariable = finalGrade(projectsCompleted, examScore);
+        window.alert(finalGradeVariable);
+
+        function finalGrade(projectsCompleted, examScore) {
+            window.alert("inside business logic");
+            let gradeCheck = 0;
+            let finalGradeReturn = "";
+
+            if (projectsCompleted > 10 || examScore > 90) {
+                gradeCheck = 3;
+            } else if (projectsCompleted >= 5 && examScore > 75) {
+                gradeCheck = 2;
+            } else if (projectsCompleted > 2 && examScore > 50) {
+                gradeCheck = 1;
+            }
+
+            if (gradeCheck === 3) {
+                finalGradeReturn = "final grade: 100";
+            } else if (gradeCheck === 2) {
+                finalGradeReturn = "final grade: 90";
+            } else if (gradeCheck === 1) {
+                finalGradeReturn = "final grade: 75";
+            } else {
+                finalGradeReturn = "final grade: 0";
+            }
+
+            return finalGradeReturn;
+
+        }
+
+   });
 
 }
-
-let finalGradeVariable = finalGrade(projectsCompleted, examScore);
-console.log(finalGradeVariable);
